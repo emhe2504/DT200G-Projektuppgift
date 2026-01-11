799a464d9bee1f03bf4f4f9803746efcd08367ee
Emma Heikkinen <emhe2504@student.miun.se>
2026-01-11 18:41:30 +0100

    Lade till article med större koppelbilder + började addera js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict"
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const dogPicButton = document.getElementById("dogpictures-big")
+    const dogPicArticle = document.getElementById("dogpictures-zoom")
+
+    if (dogPicButton) {
+        document.addEventListener("click", () => {
+
+        })
+    };
+
+});
+
+
+
